fadb9ee81716f5e012bd4a9250b99c5528ec9775
Jordi <you@example.com>
2017-11-24 09:12:55 +0100

    myfeater

--- src/de/dhbg/trinat/HelloWorld.java
+++ src/de/dhbg/trinat/HelloWorld.java
@@ -1,8 +1,11 @@
 package de.dhbg.trinat;
 
+import java.util.concurrent.SynchronousQueue;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Trinat");
 		System.out.println("Hello Trinat");
 
 	}
